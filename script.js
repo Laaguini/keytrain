5fe3e19cc0555bfb26bd56e7a06235585d8d5251
Laaguini <vovanlihoi@gmail.com>
2022-03-18 18:05:33 +0300

    fix some bugs and now you can see your accuracy

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const textContainer = document.getElementById("text");
 const textGenerator = document.getElementById("textGenerator");
 const caret = document.getElementById("caret");
 const speed = document.getElementById("speed");
+const accuracy = document.getElementById("accuracy");
 
 const word = document.createElement("div");
 word.classList.add("word");
@@ -13,6 +14,8 @@ let currentLetter;
 let startTime;
 let timeStarted = false;
 let trueCharsCount = 0;
+let allTries = 0;
+let t;
 
 let generateText = async () => {
     textContainer.textContent = ""
@@ -24,12 +27,23 @@ let generateText = async () => {
     })
 
     let res = await req.json();
+    console.log(res.text)
 
     let text = res.text.split(" ").map(w => {
         let wt = word.cloneNode();
         w.split("").forEach(l => {
             let lt = letter.cloneNode();
-            lt.textContent = l;
+            switch(l){
+                case '\u8212':
+                    lt.textContent = '-'
+                    break;
+                case 'ё':
+                    lt.textContent = 'е';
+                    break;
+                default:
+                    lt.textContent = l;
+                    break;
+                }
             wt.append(lt);
         });
         textContainer.append(wt)
@@ -41,6 +55,12 @@ let generateText = async () => {
     caret.style.left = currentLetter.getBoundingClientRect().left + "px";
     caret.style.top = currentLetter.getBoundingClientRect().top + 3 + "px";
     timeStarted = false;
+    trueCharsCount = 0.0000001;
+    allTries = 0.0000001;
+    clearInterval(t);
+    t = setInterval(() => {
+        speed.textContent = Math.floor(trueCharsCount * 60 * 1000 / (Date.now() - startTime));
+    }, 300)
 }
 
 class LetterEvents {
@@ -51,6 +71,7 @@ class LetterEvents {
     static colorNextLetter(key, code){
         let e = this.w.children[this.wc].children;
         if("qwertyuiopasdfghjklzxcvbnm,.=.?/!:;'\"йцукенгёшщзхъфывапролджэячсмитьбю".includes(key.toLowerCase())){
+            allTries++;
             if(key === e[this.lc].textContent){
                 e[this.lc].classList.add("correct");
                 currentLetter = e[this.lc];
@@ -59,14 +80,16 @@ class LetterEvents {
                 this.lc += 1;
                 trueCharsCount++;
             } else {
-                e[this.lc].classList.add("wrong")
+                e[this.lc].classList.add("wrong");
             }
         } else if((key === " ") && (this.lc === e.length)){
+            allTries++;
             this.lc = 0;
             this.wc += 1;
-            caret.style.left = currentLetter.getBoundingClientRect().left + currentLetter.offsetWidth + 10 + "px";
+            caret.style.left = currentLetter.getBoundingClientRect().left + currentLetter.offsetWidth + 6 + "px";
+            trueCharsCount++;
         }
-    }
+    }С
 }
 generateText();
 
@@ -80,8 +103,8 @@ window.addEventListener("keydown", (e) => {
     if(!timeStarted){
         startTime = Date.now();
         setInterval(() => {
-            speed.textContent = Math.floor(trueCharsCount * 60 * 60 * 10 / (Date.now() - startTime) % (60 * 60 * 1000));
-        }, 1000)
+            accuracy.textContent = (100 * trueCharsCount / allTries).toFixed(1);
+        }, 300)
         timeStarted = true;
     }
 });
